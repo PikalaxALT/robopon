a6bc1cbd539592da2fe751012c2fc13e4a42f7a0
PikalaxALT <PikalaxALT@users.noreply.github.com>
2026-02-22 17:43:59 -0500

    ssssiiiiiiiiigh

--- maps/bank_26_2.asm
+++ maps/bank_26_2.asm
@@ -5,38 +5,15 @@ INCLUDE "macros/bank20.asm"
 SECTION "Bank 26, 2", ROMX
 	script_library 26_2
 
-Func_9a168:: ; 9a168
-	ret
-
-Func_9a169:: ; 9a169
-	ret
-
-Func_9a16a:: ; 9a16a
-	ret
-
-Func_9a16b:: ; 9a16b
-	ret
-
-Func_9a16c:: ; 9a16c
-	ret
-
-Func_9a16d:: ; 9a16d
-	ret
-
-Func_9a16e:: ; 9a16e
-	ret
-
-Func_9a16f:: ; 9a16f
-	ret
-
-Func_9a170:: ; 9a170
-	ret
-
-Func_9a171:: ; 9a171
-	ret
-
-Func_9a172:: ; 9a172
-	ret
-
-Func_9a173:: ; 9a173
-	ret
+INCLUDE "maps/scripts/unknown_9a168.asm"
+INCLUDE "maps/scripts/unknown_9a169.asm"
+INCLUDE "maps/scripts/unknown_9a16a.asm"
+INCLUDE "maps/scripts/unknown_9a16b.asm"
+INCLUDE "maps/scripts/unknown_9a16c.asm"
+INCLUDE "maps/scripts/unknown_9a16d.asm"
+INCLUDE "maps/scripts/unknown_9a16e.asm"
+INCLUDE "maps/scripts/unknown_9a16f.asm"
+INCLUDE "maps/scripts/unknown_9a170.asm"
+INCLUDE "maps/scripts/unknown_9a171.asm"
+INCLUDE "maps/scripts/unknown_9a172.asm"
+INCLUDE "maps/scripts/unknown_9a173.asm"
