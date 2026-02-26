04e60d2444fb353f87a33c3592533499ea19ca8f
PikalaxALT <PikalaxALT@users.noreply.github.com>
2026-02-26 18:05:07 -0500

    Func_ad122

--- maps/bank_2b.asm
+++ maps/bank_2b.asm
@@ -6,46 +6,246 @@ SECTION "Bank 2b", ROMX
 	script_library 2b
 
 Data_ad0b4:
-	db $06, $0c, $04, $01, $ff, $ff, $ff, $ff, $ff, $ff, $2e, $07, $05, $02, $01, $1e
-	db $0d, $0d, $0f, $0d, $0e, $2e, $04, $06, $0b, $17, $69, $24, $27, $7a, $05, $0f
-	db $00, $10, $0a, $00, $04, $06, $0b, $17, $31, $24, $27, $7a, $06, $1e, $00, $11
-	db $0a, $00, $04, $06, $0b, $17, $43, $24, $27, $7a, $06, $07, $00, $10, $0a, $00
-	db $c1, $02, $ff, $ff, $ff, $00, $06, $0b, $01, $01, $00, $04, $00, $2b, $5f, $48
-	db $f4, $50, $ff, $00, $09, $0b, $01, $01, $00, $04, $00, $2b, $5f, $48, $f4, $50
-	db $19, $0c, $08, $05, $01, $01, $02, $04, $00, $2b, $00, $00, $00, $00
+	warpdef $06, $0c, $04, $01, MAP_N_A, $ff, $ff, $ff, $ff, $2e
+	warpdef $07, $05, $02, $01, MAP_30_13, $0d, $0f, $0d, $0e, $2e
+
+Data_ad0ca:
+	wildbot $04, $06, $0b, $17, $69, $24, $27, $7a, $05, $0f, $00, $10, $0a, $00
+	wildbot $04, $06, $0b, $17, $31, $24, $27, $7a, $06, $1e, $00, $11, $0a, $00
+	wildbot $04, $06, $0b, $17, $43, $24, $27, $7a, $06, $07, $00, $10, $0a, $00
+
+Data_ad0f4:
+	dw $2c1
+	dw -1
+
+Data_ad0f8:
+	person_event $ff, $00, $06, $0b, $01, $01, $00, $04, $00, PrintTextFacePlayer_2b, Data_ad0f4
+	person_event $ff, $00, $09, $0b, $01, $01, $00, $04, $00, PrintTextFacePlayer_2b, Data_ad0f4
+
+Data_ad114:
+	person_event $19, $0c, $08, $05, $01, $01, $02, $04, $00, NULL, NULL
 
 Func_ad122:: ; ad122
-	db $3e, $0f, $ea, $e2, $c7, $3e, $03, $cd, $01, $4d, $1e, $03, $21, $ca, $50, $cd
-	db $24, $4d, $fa, $df, $c7, $fe, $1e, $c2, $52, $51, $fa, $e0, $c7, $fe, $0e, $c2
-	db $52, $51, $1e, $01, $21, $14, $51, $cd, $fb, $40, $cd, $25, $52, $c3, $c5, $51
-	db $1e, $02, $21, $b4, $50, $cd, $3a, $40, $1e, $02, $21, $f8, $50, $cd, $fb, $40
-	db $21, $34, $00, $cd, $2e, $46, $b7, $c2, $8a, $51, $af, $cd, $e6, $4e, $3e, $01
-	db $cd, $c6, $4c, $af, $cd, $53, $46, $cd, $a2, $4e, $3e, $65, $cd, $fe, $4e, $cd
-	db $1c, $40, $cd, $c6, $51, $c3, $c5, $51, $21, $c3, $00, $cd, $2e, $46, $b7, $c2
-	db $b8, $51, $fa, $df, $c7, $fe, $1e, $c2, $b8, $51, $21, $35, $00, $cd, $2e, $46
-	db $b7, $c2, $b8, $51, $cd, $1c, $40, $cd, $02, $53, $af, $cd, $e6, $4e, $3e, $13
-	db $cd, $e6, $4e, $c3, $c5, $51, $3e, $01, $cd, $c6, $4c, $3e, $13, $cd, $e6, $4e
-	db $cd, $1c, $40, $c9, $3e, $65, $cd, $fe, $4e, $0e, $05, $1e, $02, $3e, $14, $cd
-	db $76, $41, $1e, $05, $3e, $07, $cd, $e4, $41, $cd, $d5, $41, $3e, $65, $cd, $fe
-	db $4e, $0e, $01, $1e, $01, $3e, $14, $cd, $76, $41, $1e, $05, $3e, $07, $cd, $e4
-	db $41, $cd, $d5, $41, $cd, $8b, $41, $3e, $65, $cd, $fe, $4e, $21, $28, $01, $cd
-	db $a9, $44, $cd, $b1, $4e, $2e, $0a, $e5, $0e, $09, $1e, $02, $3e, $1f, $cd, $ff
-	db $4d, $c1, $c9, $08, $08, $ff, $ff, $07, $08, $ff, $ff, $08, $0c, $ff, $ff, $07
-	db $0c, $ff, $ff, $1e, $00, $af, $cd, $3d, $41, $af, $cd, $53, $46, $cd, $a2, $4e
-	db $cd, $1c, $40, $21, $78, $00, $cd, $8f, $46, $3e, $65, $cd, $fe, $4e, $0e, $05
-	db $1e, $02, $3e, $14, $cd, $76, $41, $1e, $05, $3e, $07, $cd, $e4, $41, $cd, $d5
-	db $41, $cd, $8b, $41, $21, $3c, $00, $cd, $8f, $46, $1e, $01, $af, $cd, $3d, $41
-	db $01, $15, $52, $1e, $2b, $af, $cd, $88, $46, $cd, $0c, $42, $1e, $00, $af, $cd
-	db $95, $40, $21, $1e, $00, $cd, $8f, $46, $3e, $65, $cd, $fe, $4e, $0e, $05, $1e
-	db $02, $3e, $14, $cd, $76, $41, $1e, $05, $3e, $07, $cd, $e4, $41, $cd, $d5, $41
-	db $cd, $8b, $41, $21, $3c, $00, $cd, $8f, $46, $3e, $02, $cd, $77, $46, $3e, $01
-	db $cd, $53, $46, $3e, $65, $cd, $fe, $4e, $0e, $01, $1e, $01, $3e, $14, $cd, $76
-	db $41, $1e, $05, $3e, $07, $cd, $e4, $41, $cd, $d5, $41, $cd, $8b, $41, $0e, $01
-	db $11, $19, $52, $3e, $2b, $cd, $3e, $46, $01, $1d, $52, $1e, $2b, $af, $cd, $f7
-	db $41, $0e, $01, $11, $21, $52, $3e, $2b, $cd, $3e, $46, $cd, $0c, $42, $cd, $b1
-	db $4e, $3e, $0f, $cd, $f1, $4f, $af, $cd, $e6, $4e, $3e, $01, $cd, $1a, $50, $af
-	db $cd, $e6, $4e, $2e, $05, $e5, $0e, $04, $1e, $02, $af, $cd, $ff, $4d, $c1, $c9
-	db $2e, $06, $e5, $0e, $19, $1e, $01, $3e, $19, $cd, $ff, $4d, $c1, $c9
+	ld a, $0f
+	ld [wc7e2], a
+	ld a, $03
+	scall Func_80d01
+	ld e, $03
+	ld hl, Data_ad0ca
+	scall LoadEncounters
+	ld a, [wBackupMapGroup]
+	cp $1e
+	jp nz, .asm_ad152
+	ld a, [wBackupMapNumber]
+	cp $0e
+	jp nz, .asm_ad152
+	ld e, $01
+	ld hl, Data_ad114
+	scall LoadMapObjects
+	call Func_ad225
+	jp .asm_ad1c5
+.asm_ad152:
+	ld e, $02
+	ld hl, Data_ad0b4
+	scall LoadWarps
+	ld e, $02
+	ld hl, Data_ad0f8
+	scall LoadMapObjects
+	ld hl, $0034
+	scall CheckEventFlag
+	or a
+	jp nz, .asm_ad18a
+	xor a
+	scall PlayMusic
+	ld a, $01
+	scall LoadPlayerSprite
+	xor a
+	scall Func_80653
+	scall StartShakingScreen
+	ld a, $65
+	scall PlaySFX
+	scall Func_8001c
+	call Func_ad1c6
+	jp .asm_ad1c5
+.asm_ad18a:
+	ld hl, $00c3
+	scall CheckEventFlag
+	or a
+	jp nz, .asm_ad1b8
+	ld a, [wBackupMapGroup]
+	cp $1e
+	jp nz, .asm_ad1b8
+	ld hl, $0035
+	scall CheckEventFlag
+	or a
+	jp nz, .asm_ad1b8
+	scall Func_8001c
+	call Func_ad302
+	xor a
+	scall PlayMusic
+	ld a, $13
+	scall PlayMusic
+	jp .asm_ad1c5
+.asm_ad1b8:
+	ld a, $01
+	scall LoadPlayerSprite
+	ld a, $13
+	scall PlayMusic
+	scall Func_8001c
+.asm_ad1c5:
+	ret
+
+Func_ad1c6:
+	ld a, $65
+	scall PlaySFX
+	ld c, $05
+	ld e, $02
+	ld a, $14
+	scall LoadEmote
+	ld e, $05
+	ld a, $07
+	scall MoveEmote
+	scall WaitEmote
+	ld a, $65
+	scall PlaySFX
+	ld c, $01
+	ld e, $01
+	ld a, $14
+	scall LoadEmote
+	ld e, $05
+	ld a, $07
+	scall MoveEmote
+	scall WaitEmote
+	scall HideEmote
+	ld a, $65
+	scall PlaySFX
+	ld hl, $0128
+	scall PrintTextWithNPCName
+	scall StopShakingScreen
+	ld l, $0a
+	push hl
+	ld c, $09
+	ld e, $02
+	ld a, $1f
+	scall Func_80dff
+	pop bc
+	ret
+
+Data_ad215:
+	db $08, $08, $ff, $ff
+
+Data_ad219:
+	db $07, $08, $ff, $ff
+
+Data_ad21d:
+	db $08, $0c, $ff, $ff
+
+Data_ad221:
+	db $07, $0c, $ff, $ff
+
+Func_ad225:
+	ld e, $00
+	xor a
+	scall SetPersonVisibilityState
+	xor a
+	scall Func_80653
+	scall StartShakingScreen
+	scall Func_8001c
+	ld hl, $0078
+	scall ScriptSleep
+	ld a, $65
+	scall PlaySFX
+	ld c, $05
+	ld e, $02
+	ld a, $14
+	scall LoadEmote
+	ld e, $05
+	ld a, $07
+	scall MoveEmote
+	scall WaitEmote
+	scall HideEmote
+	ld hl, $003c
+	scall ScriptSleep
+	ld e, $01
+	xor a
+	scall SetPersonVisibilityState
+	ld bc, Data_ad215
+	ld e, $2b
+	xor a
+	scall MovePersonAndWait
+	scall WaitNPCStep
+	ld e, $00
+	xor a
+	scall SpriteFace
+	ld hl, $001e
+	scall ScriptSleep
+	ld a, $65
+	scall PlaySFX
+	ld c, $05
+	ld e, $02
+	ld a, $14
+	scall LoadEmote
+	ld e, $05
+	ld a, $07
+	scall MoveEmote
+	scall WaitEmote
+	scall HideEmote
+	ld hl, $003c
+	scall ScriptSleep
+	ld a, $02
+	scall PlayerFace
+	ld a, $01
+	scall Func_80653
+	ld a, $65
+	scall PlaySFX
+	ld c, $01
+	ld e, $01
+	ld a, $14
+	scall LoadEmote
+	ld e, $05
+	ld a, $07
+	scall MoveEmote
+	scall WaitEmote
+	scall HideEmote
+	ld c, $01
+	ld de, Data_ad219
+	ld a, $2b
+	scall MovePlayer
+	ld bc, Data_ad21d
+	ld e, $2b
+	xor a
+	scall MovePerson
+	ld c, $01
+	ld de, Data_ad221
+	ld a, $2b
+	scall MovePlayer
+	scall WaitNPCStep
+	scall StopShakingScreen
+	ld a, $0f
+	scall FadeOutAudio
+	xor a
+	scall PlayMusic
+	ld a, $01
+	scall FadeInAudio
+	xor a
+	scall PlayMusic
+	ld l, $05
+	push hl
+	ld c, $04
+	ld e, $02
+	xor a
+	scall Func_80dff
+	pop bc
+	ret
+Func_ad302:
+	ld l, $06
+	push hl
+	ld c, $19
+	ld e, $01
+	ld a, $19
+	scall Func_80dff
+	pop bc
+	ret
 
 Func_ad310:
 	ret
